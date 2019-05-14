9f44c7b6f702d9cd58fe40f279c6194cb9a17729
Yang <yang.shen@commscope.com>
2019-05-14 17:20:01 +0800

    init 679 pycode

--- problems/679/features/steps/step_pb679.py
+++ problems/679/features/steps/step_pb679.py
@@ -1,5 +1,6 @@
 from behave import given, when, then, step
 from  pb679 import Solution
+from fractions import Fraction
 
 @given(u'Input {nums_string}')
 def step_impl(step, nums_string):
@@ -13,4 +14,6 @@ def step_impl(step):
 
 @then(u'Get Result {validation}')
 def step_impl(step, validation):
+    #validate_math = 4 / (1 - Fraction(2,3))
+    #print('4 / (1 - 2/3): ', validate_math)
     assert str(step.calculated_result) == (validation)
--- problems/679/pb679.py
+++ problems/679/pb679.py
@@ -1,4 +1,5 @@
 import pysnooper
+from fractions import Fraction
 
 class Solution:
 
@@ -6,15 +7,36 @@ class Solution:
         return 24
 
     @pysnooper.snoop()
-    def check_validation(self, nums):
-        return False    
-    
-    @pysnooper.snoop()
-    def judgePoint24(self, nums) -> int:
+    def check_validation(self, nums, expected):
         num_count = len(nums)
         if num_count < 1:
             return False
         if num_count == 1:
-            return nums[0] == self.EXPECTED_CONST()
+            return nums[0] == expected
+        validation = False
 
-        return self.check_validation(nums)
+        new_expected = expected + nums[0]
+        validation = validation or self.check_validation(nums[1:], new_expected)
+
+        new_expected = expected - nums[0]
+        validation = validation or self.check_validation(nums[1:], new_expected)
+
+        new_expected = nums[0] - expected
+        validation = validation or self.check_validation(nums[1:], new_expected)
+
+        new_expected = expected * nums[0]
+        validation = validation or self.check_validation(nums[1:], new_expected)
+
+        if nums[0] != 0:
+            new_expected = Fraction(expected, nums[0]) 
+            validation = validation or self.check_validation(nums[1:], new_expected)  
+
+        if expected != 0:
+            new_expected = Fraction(nums[0], expected) 
+            validation = validation or self.check_validation(nums[1:], new_expected)  
+
+        return validation    
+    
+    @pysnooper.snoop()
+    def judgePoint24(self, nums) -> int:
+        return self.check_validation(nums, self.EXPECTED_CONST())
